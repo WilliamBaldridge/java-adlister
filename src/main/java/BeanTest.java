61ee3b732c2f66027d5db9ae73b0e2e034640ad9
WilliamBaldridge <baldridge.william09@gmail.com>
2021-04-12 16:34:43 -0500

    Javabeans lecture and exercise

---
+++ src/main/java/BeanTest.java
@@ -0,0 +1,45 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+public class BeanTest {
+
+
+
+
+    public static void main(String[] args) {
+
+
+//        String dbURL = "jdbc:mysql://localhost:3306/design_test_db";
+//        String username = "";
+//        String password = "";
+//
+//        try (Connection conn = DriverManager.getConnection(dbURL, username, password)) {
+//
+//            // code to execute SQL queries goes here...
+//            String sql = "INSERT INTO authors (author_first_name, author_last_name) VALUES (?, ?)";
+//
+//            PreparedStatement statement = conn.prepareStatement(sql);
+//            statement.setString(1, "Billy");
+//            statement.setString(2, "Bob");
+//
+//        } catch (SQLException ex) {
+//            ex.printStackTrace();
+//        }
+//
+
+
+
+        Album album1 = new Album();
+
+        Author author1 = new Author();
+
+//        Quote quote1 = new Quote();
+
+
+    }
+
+}
---
+++ src/main/java/Quote.java
@@ -0,0 +1,35 @@
+public class Quote {
+
+private int id;
+private String content;
+private String author;
+
+
+    public Quote() {
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
